15081de3378c3386dd543cc86fc5d11cb2eb5724
levinrobin <levinpaktolus@gmail.com>
2026-03-26 06:44:34 +0000

    added user shell script

---
+++ scripts/create-user.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+USER_NAME="user_01"
+USER_PASSWORD="Levin16robert@"
+
+echo "Checking if user '$USER_NAME' already exists..."
+
+psql -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" <<EOF
+DO \$\$
+BEGIN
+  IF NOT EXISTS (
+     SELECT 1 FROM pg_roles WHERE rolname = '$USER_NAME'
+  ) THEN
+     CREATE USER $USER_NAME WITH PASSWORD '$USER_PASSWORD';
+     RAISE NOTICE 'User created successfully.';
+  ELSE
+     RAISE NOTICE 'User already exists. Skipping creation.';
+  END IF;
+END
+\$\$;
+EOF
+
+echo "Script execution completed."
